46318d8cc26ffae8fdf7f407265430c7c6e760c7
Fedor Zhyvel <fedor.zhyvel@geomotiv.com>
2024-04-18 14:11:16 +0300

    profile page redesign

--- lib/presentation/profile_page/profile_page.dart
+++ lib/presentation/profile_page/profile_page.dart
@@ -13,8 +13,141 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('User page coming soon'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(
+              left: 16,
+              right: 16,
+              bottom: 24,
+              top: 8,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 48,
+                  child: Icon(Icons.person),
+                ),
+                SizedBox(width: 16),
+                Text('Name'),
+              ],
+            ),
+          ),
+          Container(
+            color: Colors.grey[100],
+            child: Column(
+              children: [
+                const ListElement(
+                  headerText: 'Ваш пол',
+                  title: 'Мужской',
+                ),
+                const Divider(
+                  height: 0,
+                  color: Colors.black,
+                  thickness: 0.2,
+                ),
+                const ListElement(
+                  headerText: 'Не указано',
+                  title: 'Изменить email',
+                ),
+                const Divider(
+                  height: 0,
+                  color: Colors.black,
+                  thickness: 0.2,
+                ),
+                const ListElement(
+                  headerText: 'Цветовая тема',
+                  title: 'Светлая',
+                ),
+                const Divider(
+                  height: 0,
+                  color: Colors.black,
+                  thickness: 0.2,
+                ),
+                const ListElement(
+                  headerText: 'Подтверждение заказов',
+                  title: 'По коду, лицу или отпечатку',
+                ),
+                const Divider(
+                  height: 0,
+                  color: Colors.black,
+                  thickness: 0.2,
+                ),
+                const ListElement(
+                  headerText: 'Настройка уведомлений',
+                ),
+                const Divider(
+                  height: 0,
+                  color: Colors.black,
+                  thickness: 0.2,
+                ),
+                const ListElement(
+                  headerText: 'Мои реквизиты',
+                ),
+                const Divider(
+                  height: 0,
+                  color: Colors.black,
+                  thickness: 0.2,
+                ),
+                ListElement(
+                  headerText: 'Выйти из аккаунта',
+                  headerTextStyle: TextStyle(color: Colors.red[400]),
+                ),
+                const Divider(
+                  height: 0,
+                  color: Colors.black,
+                  thickness: 0.2,
+                ),
+                const ListElement(
+                  headerText: 'Удалить личный кабинет',
+                  headerTextStyle: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ListElement extends StatelessWidget {
+  const ListElement({
+    super.key,
+    required this.headerText,
+    this.headerTextStyle,
+    this.title,
+  });
+  final String headerText;
+  final TextStyle? headerTextStyle;
+  final String? title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: title == null ? 10 : 2,
+      ),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                headerText,
+                style: headerTextStyle,
+              ),
+              if (title != null)
+                Text(
+                  title!,
+                  style: TextStyle(color: Colors.grey[700]),
+                ),
+            ],
+          ),
+        ],
       ),
     );
   }
